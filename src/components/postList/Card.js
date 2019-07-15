bc8d7a52d72a7719c59db3084d0a914ee77683ae
BangHyeonJun <bhj2849@naver.com>
2019-07-15 23:42:31 +0900

    카드 스타일 변경

--- src/components/postList/Card.js
+++ src/components/postList/Card.js
@@ -3,7 +3,10 @@ import noImg from "../../static/images/no_image.png";
 import { Link } from "react-router-dom";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./Card.scss";
+import styles from "./Card.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 const Card = ({ id, img, title, publish_date, category, commentCount }) => {
     let pub_date = (() => {
@@ -19,33 +22,41 @@ const Card = ({ id, img, title, publish_date, category, commentCount }) => {
     })();
 
     return (
-        <Link to={`post/${id}/`} className="a-Lnik">
-            <div className="post-list-card-container">
-                <div className="post-list-card">
-                    <div className="post-list-front">
-                        <div className="post-list-img-container">
+        <Link to={`post/${id}/`} className={cx("a-Lnik")}>
+            <div className={cx("post-list-card-container")}>
+                <div className={cx("post-list-card")}>
+                    <div className={cx("post-list-front")}>
+                        <div className={cx("post-list-img-container")}>
                             {img.path ? (
                                 <img src={`http://localhost:4000${img.path}`} />
                             ) : (
                                 <img src={noImg} />
                             )}
                         </div>
-                        <div className="post-list-main-container">
-                            <div className="post-list-publish-date-container">
+                        <div className={cx("post-list-main-container")}>
+                            <div
+                                className={cx(
+                                    "post-list-publish-date-container"
+                                )}
+                            >
                                 <h5>{pub_date}</h5>
                             </div>
-                            <div className="post-list-title-container">
+                            <div className={cx("post-list-title-container")}>
                                 <h3>{title}</h3>
                             </div>
-                            <div className="post-list-detail-info-container">
+                            <div
+                                className={cx(
+                                    "post-list-detail-info-container"
+                                )}
+                            >
                                 <h5>자세히 보기</h5>
                                 <FontAwesomeIcon icon={faArrowRight} />
                             </div>
                         </div>
                     </div>
-                    <div className="post-list-back">
+                    <div className={cx("post-list-back")}>
                         <div>{category}</div>
-                        <div className="post-list-detail-info-container">
+                        <div className={cx("post-list-detail-info-container")}>
                             <h5>자세히 보기</h5>
                             <FontAwesomeIcon icon={faArrowRight} />
                         </div>
